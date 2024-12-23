68f51fcc35bdc0c3cd82073cf96f47a182aa45f9
Joanne Phan <phankhietvan04@gmail.com>
2024-12-23 02:26:17 -0500

    Projects added

---
+++ data/project.js
@@ -0,0 +1,26 @@
+window.project = [
+    {
+        id: 1,
+        name: "Library Search Form",
+        description: "In-class assignment, creating a form for school department",
+        URL: "https://github.com/khietvan123/library_search_form",
+        deployURL: "https://khietvan123.github.io/library_search_form/",
+        img: ""
+    },
+    {
+        id: 2,
+        name: "Music app",
+        description: "In-class assignment, creating a website that shows your favorite artists and their music",
+        URL: "https://github.com/khietvan123/Pop_Music_app",
+        deployURL: "https://khietvan123.github.io/Pop_Music_app/",
+        img: ""
+    },
+    {
+        id: 3,
+        name: "Personal Profile",
+        description: "Personal project to create a website that has",
+        URL: "https://github.com/khietvan123/Pop_Music_app",
+        deployURL: "https://khietvan123.github.io/Pop_Music_app/",
+        img: ""
+    }
+];
---
+++ index.js
@@ -0,0 +1,41 @@
+const projects = window.project;
+document.addEventListener("DOMContentLoaded", () => {
+    const container = document.querySelector('#project-container');
+
+    if (projects.length > 0) {
+        projects.forEach((project) => {
+            const newCard = createProjectCard(project);
+            container.appendChild(newCard);
+        });
+    }
+
+    function createProjectCard(project) {
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        const imgPrj = document.createElement("img");
+        imgPrj.classList.add("card-img");
+        imgPrj.src = project.img || "./img/project-img.jpg";
+        imgPrj.alt = project.name || "Project image";
+
+        const cardContent = document.createElement("div");
+        cardContent.classList.add("card-content");
+
+        const cardName = document.createElement("a");
+        cardName.href = project.URL;
+        cardName.textContent = project.name || "Unnamed Project";
+        cardName.classList.add("card-name");
+
+        const cardDescription = document.createElement("p");
+        cardDescription.textContent = project.description || "No description available.";
+        cardDescription.classList.add("card-description");
+
+        cardContent.appendChild(cardName);
+        cardContent.appendChild(cardDescription);
+
+        card.appendChild(imgPrj);
+        card.appendChild(cardContent);
+
+        return card;
+    }
+});
